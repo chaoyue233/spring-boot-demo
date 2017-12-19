784fcf75a21e02590628476f588c1f50d05f682a
chaoyue.wu <chaoyue.wu@weimob.com>
2017-12-19 14:26:12 +0800

    add spring security basic

---
+++ src/test/java/chaoyue/hello/GreetingApplicationTest.java
@@ -0,0 +1,15 @@
+package chaoyue.hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = GreetingApplication.class)
+public class GreetingApplicationTest {
+  @Test
+  public void contextLoads(){
+
+  }
+}
